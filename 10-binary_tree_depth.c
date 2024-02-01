126b3f224119353776783f20d360a57420d95414
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-01 14:27:54 +0200

    lets try

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+
+/**
+ * binary_tree_height_wrapper - checks if the node is a root
+ * @tree: node which is the root initially
+ * @count: node which is the root initially
+ *
+ * Return: None
+ */
+size_t binary_tree_depth_wrapper(const binary_tree_t *tree, int count)
+{
+	if (tree->parent == NULL)
+	{			
+        return (count);
+	}
+	return (binary_tree_depth_wrapper(tree->parent, count + 1));
+}
+
+
+/**
+ * binary_tree_height - checks if the node is a root
+ * @tree: node which is the root initially
+ *
+ * Return: None
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return (binary_tree_depth_wrapper(tree, 0));
+}
