81eeb9fb3b8494a25a57f099d243952731896931
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-01 12:31:43 +0200

    push

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_leaf - checks if the node is a leaf
+ * @node: node which is the root initially
+ *
+ * Return: length of printed tree after process
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
