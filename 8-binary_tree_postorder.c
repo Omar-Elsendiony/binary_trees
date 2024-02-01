19adc30438a0fb7d2b91e09878114a7ae0d7b504
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-01 12:49:25 +0200

    beee

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_postorder - checks if the node is a root
+ * @tree: node which is the root initially
+ * @func: address of a function to be used (print)
+ *
+ * Return: None
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+}
