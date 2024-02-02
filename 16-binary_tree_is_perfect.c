38cbab1019f81e8acd626a5c4a40aff7b063d4ad
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-02 04:02:44 +0200

    16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * max - get maximum of two numbers
+ * @x: first number
+ * @y: second number
+ *
+ * Return: maximum of both integers
+ */
+int max(int x, int y)
+{
+	if (x > y)
+		return (x);
+	else
+		return (y);
+}
+
+/**
+ * binary_tree_height_wrapper - get maximum of two numbers
+ * @tree: first number
+ *
+ * Return: maximum of both integers
+ */
+size_t binary_tree_height_wrapper(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (1 + max(binary_tree_height_wrapper(tree->left),
+				binary_tree_height_wrapper(tree->right)));
+}
+
+
+/**
+ * binary_tree_is_perfect - checks if the node is a root
+ * @tree: node which is the root initially
+ *
+ * Return: None
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t l, r;
+	int diff;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l = binary_tree_height_wrapper(tree->left);
+	r = binary_tree_height_wrapper(tree->right);
+	diff = l == r;
+	return (diff);
+}
